ba1b0464d7d700e242792f1028b1f7cb755474fc
nicogorlo <gorlon@ethz.ch>
2023-04-25 16:44:38 +0200

    added visualization util

--- isar/util/isar_utils.py
+++ isar/util/isar_utils.py
@@ -2,7 +2,9 @@ from time import perf_counter_ns
 import itertools
 import os
 import cv2
-
+import random
+import numpy as np
+from PIL import Image
 
 class performance_measure:
 
@@ -26,3 +28,27 @@ def get_image_it_from_folder(datadir) -> itertools.cycle:
     images = itertools.cycle([image for image in sorted(os.listdir(datadir))])
     
     return images
+
+
+
+## visualization:
+
+def semantic_obs_to_img(semantic_obs, semantic_palette):
+    semantic_img = Image.new("P", (semantic_obs.shape[1], semantic_obs.shape[0]))
+    semantic_img.putpalette(semantic_palette.flatten())
+    semantic_img.putdata((semantic_obs.flatten()).astype(np.uint8))
+    semantic_img = semantic_img.convert('RGB')
+    semantic_img = cv2.cvtColor(np.asarray(semantic_img), cv2.COLOR_BGR2RGB)
+    return semantic_img
+
+def generate_pastel_color():
+    """
+    Generate a random pastel color.
+    """
+    # Generate random RGB values with a specific range
+    r, g, b = [random.randint(64, 255) for _ in range(3)]
+    # Calculate the average of the RGB values
+    average = (r + g + b) // 3
+    # Calculate the new RGB values as a percentage of the average
+    r, g, b = [int((x + average) // 2) for x in (r, g, b)]
+    return (r, g, b)
