83a2fd0e4f2dab7f82b0afa2744e37ebbee1e3e2
nicogorlo <gorlon@student.ethz.ch>
2023-06-06 18:29:42 +0200

    fixed model weight download

--- isar/util/download_model_weights.py
+++ isar/util/download_model_weights.py
@@ -2,15 +2,16 @@ import gdown
 import requests
 import os
 
-if not os.path.exists('../modelzoo'):
-    os.makedirs('../modelzoo')
+dirname = os.path.dirname(__file__)
+if not os.path.exists(os.path.join(dirname, '../modelzoo')):
+    os.makedirs(os.path.join(dirname, '../modelzoo'))
 
-if not os.path.exists('../feature_dict_dinov2'):
-    os.makedirs('../feature_dict_dinov2')
+if not os.path.exists(os.path.join(dirname, '../feature_dict_dinov2')):
+    os.makedirs(os.path.join(dirname, '../feature_dict_dinov2'))
 
 r = requests.get('https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth', allow_redirects=True)
-open('../modelzoo/sam_vit_h_4b8939.pth', 'wb').write(r.content)
+open(os.path.join(dirname, '../modelzoo/sam_vit_h_4b8939.pth'), 'wb').write(r.content)
 
 
 gdown.download('https://drive.google.com/uc?id=15nlfT0DwwEBQwNzS_gxs5SwMZwDf0lLE&export=download', 
-               '../feature_dict_dinov2/dino_features_dinov2_vitl14.pkl', quiet=False)
+               os.path.join(dirname, '../feature_dict_dinov2/dino_features_dinov2_vitl14.pkl'), quiet=False)
