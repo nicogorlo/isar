ecfd4cbdbd045913eedfa1ad0ee962368b7565b8
nicogorlo <gorlon@ethz.ch>
2023-06-06 18:20:29 +0200

    added dino feature dict to download script

--- isar/util/download_model_weights.py
+++ isar/util/download_model_weights.py
@@ -10,3 +10,7 @@ if not os.path.exists('../feature_dict_dinov2'):
 
 r = requests.get('https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth', allow_redirects=True)
 open('../modelzoo/sam_vit_h_4b8939.pth', 'wb').write(r.content)
+
+
+gdown.download('https://drive.google.com/uc?id=15nlfT0DwwEBQwNzS_gxs5SwMZwDf0lLE&export=download', 
+               'feature_dict_dinov2/dino_features_dinov2_vitl14.pkl', quiet=False)
